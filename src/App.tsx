0decc078cb7b39481bc6a41de1cbee2f828fa3c1
Amir Salgado <gigas@192.168.1.8>
2022-09-27 11:55:22 -0500

    setup functions for vinyl calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { MantineProvider } from '@mantine/core';
+import Calc from './pages/calc';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MantineProvider>
+      <div className="App">
+        <h1>Calculadora de Vinyl</h1>
+        <Calc />
+      </div>
+    </MantineProvider>
+  )
 }
-
-export default App;
